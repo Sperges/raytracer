4b8e762b00f58f913328a898b660a2bff1232a4f
rnbiv <rnbiv45@gmail.com>
2021-12-29 04:31:39 -0500

    add rays file

---
+++ src/ray.rs
@@ -0,0 +1,19 @@
+use crate::prelude::*;
+
+pub struct Ray {
+    orig: Point3,
+    dir: Vec3,
+}
+
+impl Ray {
+    pub fn new(orig: Point3, dir: Vec3) -> Self {
+        Self {
+            orig,
+            dir,
+        }
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        self.orig + t * self.dir
+    }
+}
